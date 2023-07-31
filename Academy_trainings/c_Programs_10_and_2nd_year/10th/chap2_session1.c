6a95dbb675c7e7b69bddfed808fd36787863245b
Zeshan Ali <zishraj07@gmail.com>
2023-07-31 21:44:30 +0500

    updating c folder and adding chap2

---
+++ Academy_trainings/c_Programs_10_and_2nd_year/10th/chap2_session1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int age = 29;
+    float height = 3.7;
+    char grade = 'A';
+    printf("%d the age is: ",age);
+    printf("\nthe height is: %f",height);
+    printf("\nthe grade is:%c",grade);
+
+    printf("the age is %d, the hehight is %f, the grade is %c",age,height,grade);
+}
