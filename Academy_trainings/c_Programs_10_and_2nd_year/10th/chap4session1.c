8c6f7311370e1a7755d31e6b4b379587603dbf76
Zeshan Ali <zishraj07@gmail.com>
2023-08-04 22:58:55 +0500

    chap 4 array dec;aration and initi

---
+++ Academy_trainings/c_Programs_10_and_2nd_year/10th/chap4session1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{    int age=25; // varaible declaration
+    int age_of_stu[25]= {12,14,15,16,18};// array declaration
+    char grade_of_stu[20]= {'A','B',}; // array declaratioo
+     age_of_stu[22]=78;
+    printf("the value of age is:%d \n",age);
+    printf("the age of first student:%d",age_of_stu[5]);
+    printf("\n the age of second student:%d",age_of_stu[22]);
+    printf("the grade of first student:%c \n",grade_of_stu[0]);
+
+
+
+}
